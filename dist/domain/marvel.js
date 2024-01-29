51f0602cfae97fd03d386dbedd7b876983d06347
will-nakagawa <saikoukanojo@gmail.com>
2024-01-29 03:56:50 -0300

    remove dist

--- dist/domain/marvel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class Marvel {
-    constructor(id, name, description, thumbnail) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.thumbnail = thumbnail;
-    }
-}
